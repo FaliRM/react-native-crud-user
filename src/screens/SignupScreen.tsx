9fe1933bc9f7721bc2a330dd7cb8dbd76deae552
Rafael <rafael+1@bolt.global>
2020-12-21 16:25:00 +0800

    style 'SignupScreen'
    style 'SignupScreen'

--- src/screens/SignupScreen.tsx
+++ src/screens/SignupScreen.tsx
@@ -1,67 +1,71 @@
 import React, { useState } from 'react';
-import {
-  SafeAreaView,
-  View,
-  Text,
-  TextInput,
-  Button,
-  StyleSheet,
-} from 'react-native';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import { useDispatch } from 'react-redux';
 import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { Card, Button } from 'react-native-elements';
 
 import { RootStackParamList } from '_types';
-import { signup } from '_services';
+import { createuser } from '_context/actions';
+import { showAlert } from '_utils';
+import CardItem from '_components/CardItem';
+import { UNIT, textStyles, viewStyles } from '_styles';
 
-type ScreenProps = {
+interface ScreenProps {
   route: RouteProp<RootStackParamList, 'Signup'>;
   navigation: StackNavigationProp<RootStackParamList, 'Signup'>;
-};
+}
 
 const SignupScreen = (props: ScreenProps) => {
   const { navigation } = { ...props };
-  const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const { containerStyle, cardItemStyle } = styles;
+  const dispatch = useDispatch();
+  const createUser = ({ email, password }) =>
+    dispatch(createuser({ email, password }));
+
+  const handleSignup = async () => {
+    if (!email || !password) {
+      showAlert({
+        alertTitle: 'Missing Fields',
+        alertMessage: 'Please ensure to fulfill all the fields',
+      });
+    }
+
+    createUser({ email, password });
+
+    navigation.navigate('Home');
+  };
+
+  const { title } = textStyles;
+  const { buttonStyle } = viewStyles;
+  const { containerStyle, cardStyle } = styles;
 
   return (
     <SafeAreaView style={containerStyle}>
-      <Text>Signup Screen</Text>
-      <View style={cardItemStyle}>
-        <Text>Username</Text>
-        <TextInput
-          onChangeText={(username: string) => setUserName(username)}
-          value={userName}
-          placeholder="Username"
-          autoCorrect={false}
-        />
-      </View>
-      <View style={cardItemStyle}>
-        <Text>Email</Text>
-        <TextInput
+      <Text style={[title, { marginBottom: 30 * UNIT }]}>Signup Screen</Text>
+      <Card containerStyle={cardStyle}>
+        <Card.Title>SIGNUP SCREEN</Card.Title>
+        <Card.Divider />
+        <CardItem
+          label="Email"
+          placeholder="user@email.com"
           onChangeText={(email: string) => setEmail(email)}
           value={email}
-          placeholder="Email"
-          autoCorrect={false}
         />
-      </View>
-      <View style={cardItemStyle}>
-        <Text>Password</Text>
-        <TextInput
+        <CardItem
+          label="Password"
+          placeholder="******"
           onChangeText={(password: string) => setPassword(password)}
           value={password}
-          placeholder="Password"
-          autoCorrect={false}
+          secureTextEntry
         />
-      </View>
+      </Card>
       <Button
         title="Signup"
-        onPress={() => {
-          signup(userName);
-          navigation.navigate('Home');
-        }}
+        onPress={() => handleSignup()}
+        buttonStyle={buttonStyle}
       />
     </SafeAreaView>
   );
@@ -73,8 +77,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  cardItemStyle: {
-    flexDirection: 'row',
+  cardStyle: {
+    width: 360 * UNIT,
   },
 });
 
