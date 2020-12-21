92b8f3b3f12dbcac91717b65622953784163e7b5
Rafael <rafael+1@bolt.global>
2020-12-21 11:28:28 +0800

    wrap the 'App' on the Provider and use the reducers to create a store
    wrap the 'App' on the Provider and use the reducers to create a store

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from '_context/reducers';
 import AppContainer from '_navigation/AppContainer';
 
+const store = createStore(reducers, applyMiddleware(thunk));
+
 const App = () => {
-  return <AppContainer />;
+  return (
+    <Provider store={store}>
+      <AppContainer />
+    </Provider>
+  );
 };
 
 export default App;
